6f96c9e21b16647a0f52f3f2deb3ab590d920ef0
Guido A.J. Stevens <guido.stevens@cosent.nl>
2014-01-03 17:39:25 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,38 +1,42 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.1'
 
-setup(name='cultact.subsite',
-      version=version,
-      description="Subsite implementation for Plone",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='cultact.subsite',
+    version=version,
+    description="Subsite implementation for Plone",
+    long_description=open("README.txt").read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['cultact'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='Guido Stevens',
+    author_email='guido.stevens@cosent.net',
+    url='http://cosent.nl',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['cultact'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    extras_require={
+        'test': ['plone.app.testing']
+    },
+    entry_points="""
       # -*- Entry points: -*-
 
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
+)
