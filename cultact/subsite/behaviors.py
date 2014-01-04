dd5bc5a73145abb993e20e6573786d298f4893bf
Guido A.J. Stevens <guido.stevens@cosent.nl>
2014-01-04 13:03:13 +0000

    cleanup behavior

--- cultact/subsite/behaviors.py
+++ cultact/subsite/behaviors.py
@@ -1,28 +1,19 @@
-from collections import OrderedDict
-
-
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.directives import form
 from plone.supermodel import model
-from cultact.types.behaviors.core import ICultactCore  # FIXME
 from rwproperty import getproperty, setproperty
 from zope import schema
-from zope.component import adapts
 from zope.interface import alsoProvides
-from zope.interface import implements
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from cultact.subsite import _
+from cultact.subsite import subsite_config
 
-subsites = OrderedDict([('--', u'--'),
-                        ('maastricht', u'Maastricht'),
-                        ('sittard', u'Sittard'),
-                        ('other', u'Overig')])
 
 subsite_vocabulary = SimpleVocabulary(
     [
         SimpleTerm(value=k, token=k, title=v)
-        for (k, v) in subsites.items()
+        for (k, v) in subsite_config.items()
     ]
 )
 
@@ -35,29 +26,25 @@ class ISubsiteAssignment(model.Schema):
         vocabulary=subsite_vocabulary,
         title=_(
             u'label_subsite',
-            default=u'Stadsregio'
+            default=u'Subsite'
         ),
         description=_(u'help_subsite',
                       default=(u'Bepaalt in welke lokale website'
                                u' dit item getoond wordt.')),
-        required=True,
-        missing_value=subsites.keys()[0],
+        required=False,
+        missing_value=None
     )
-
-    form.order_before(subsite='ICultactCore.subjects')
 
 
 @form.default_value(field=ISubsiteAssignment['subsite'])
 def default_subsite(data):
-    return data.request.subsite
+    return data.request.get('subsite', None)
 
 
 alsoProvides(ISubsiteAssignment, IFormFieldProvider)
 
 
 class SubsiteAssignment(object):
-    implements(ISubsiteAssignment)
-    adapts(ICultactCore)
 
     def __init__(self, context):
         self.context = context
