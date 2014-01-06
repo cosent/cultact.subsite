c244fb59845d763a0b31b35a5953da9a74502e89
Guido A.J. Stevens <guido.stevens@cosent.nl>
2014-01-06 13:41:52 +0000

    provide interfaces API

---
+++ cultact/subsite/interfaces.py
@@ -0,0 +1,5 @@
+from .content import ISubsite  # flake8:noqa
+from .content import ICityhub  # flake8:noqa
+from .content import ISittardSite  # flake8:noqa
+from .content import IMaastrichtSite  # flake8:noqa
+from .content import ICode043Site  # flake8:noqa
